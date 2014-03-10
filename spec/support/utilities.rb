89e0871d1c53ce12199af62fec36986e736d6018
sunsoft22 <skrithi1@umbc.edu>
2014-03-10 07:07:08 -0400

    Add user microposts

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,11 +1,5 @@
 include ApplicationHelper
 
-def valid_signin(user)
-  fill_in "Email", with: user.email
-  fill_in "Password", with: user.password
-  click_button "Sign in"
-end
-
 RSpec::Matchers.define :have_error_message do |message|
   match do |page|
     expect(page).to have_selector('div.alert.alert-error', text: message)
@@ -20,7 +14,7 @@ def sign_in(user, options={})
     user.update_attribute(:remember_token, User.hash(remember_token))
   else
     visit signin_path
-    fill_in "Email",    with: user.email
+    fill_in "Email", with: user.email
     fill_in "Password", with: user.password
     click_button "Sign in"
   end
