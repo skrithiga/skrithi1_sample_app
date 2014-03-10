1fd635f6befc426d70e15c03340cf402e3245e8d
sunsoft22 <skrithi1@umbc.edu>
2014-03-09 21:31:13 -0400

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,8 @@ class User < ActiveRecord::Base
   uniqueness: {case_sensitive: false}
   has_secure_password
   validates :password, length: { minimum: 6 }
-
+  has_many :microposts, dependent: :destroy
+  
   def User.new_remember_token
     SecureRandom.urlsafe_base64
   end
@@ -16,10 +17,17 @@ class User < ActiveRecord::Base
   def User.hash(token)
     Digest::SHA1.hexdigest(token.to_s)
   end
+  
+  def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
 
   private
 
     def create_remember_token
       self.remember_token = User.hash(User.new_remember_token)
     end
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Skrithi1SampleApp::Application.routes.draw do
-resources :users
-resources :sessions, only: [:new, :create, :destroy]
+  resources :users
+  resources :sessions,   only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destroy]
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
   match '/help',    to: 'static_pages#help',    via: 'get'
