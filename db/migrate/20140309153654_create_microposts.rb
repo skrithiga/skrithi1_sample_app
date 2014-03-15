860e6310260ee089247ffaf866b9bd9cf2b1e97b
sunsoft22 <skrithi1@umbc.edu>
2014-03-15 18:20:08 -0400

    Final changes to db migrate

--- db/migrate/20140309153654_create_microposts.rb
+++ db/migrate/20140309153654_create_microposts.rb
@@ -1,13 +1,11 @@
-class CreateRelationships < ActiveRecord::Migration
+class CreateMicroposts < ActiveRecord::Migration
   def change
-    create_table :relationships do |t|
-      t.integer :follower_id
-      t.integer :followed_id
+    create_table :microposts do |t|
+      t.string :content
+      t.integer :user_id
 
       t.timestamps
     end
-    add_index :relationships, :follower_id
-    add_index :relationships, :followed_id
-    add_index :relationships, [:follower_id, :followed_id], unique: true
+    add_index :microposts, [:user_id,:created_at]
   end
 end
